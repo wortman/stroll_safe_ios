51b09b3c47b29b096bdbb9d3f5582cf32712b0ed
christine prince <christineprince@christine-princes-MacBook-Pro.local>
2015-03-21 13:14:06 -0500

    Broken shit

--- Stroll Safe/ViewController.swift
+++ Stroll Safe/ViewController.swift
@@ -9,18 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var progressBar: UIProgressView!
+    /*@IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var releaseButton: UIButton!
     @IBOutlet weak var thumbButton: UIButton!
     @IBOutlet weak var titleMain: UILabel!
-    @IBOutlet weak var titleSub: UILabel!
+    @IBOutlet weak var titleSub: UILabel!*/
 
+    @IBAction func releaseButtonAction(sender: UIButton) {
+    }
+    
+    @IBAction func thumbButtonAction(sender: AnyObject) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        progressBar.hidden = true
-        releaseButton.hidden = true
+        //progressBar.hidden = true
+        //releaseButton.hidden = true
         
         
     }
@@ -29,7 +34,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+   
 
 }
 
