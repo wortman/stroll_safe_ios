ad121d055b76a047c14e018a63bded544cfe87c4
Mitchel Wortman <wortman2@illinois.edu>
2015-03-21 11:46:07 -0500

    Initial Commit

---
+++ Stroll Safe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stroll Safe
+//
+//  Created by Guest User on 3/21/15.
+//  Copyright (c) 2015 Stroll Safe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
