baf6a379113fc26b6ae0e6dabcd7041dc0a9c12b
Mitchel Wortman <wortman2@illinois.edu>
2015-03-22 02:41:12 -0500

    Working on numpad tie in

--- Stroll Safe/LockdownControllerViewController.swift
+++ Stroll Safe/LockdownControllerViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class LockdownControllerViewController: UIViewController {
 
+    var input = -1
+    var passField = [Int]() //array containing the password input
+    
+/*  while (sizeof(array) < 4)
+        listen for button presses
+        chage input accordingly
+        passField.append(input)
+*/
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +29,43 @@ class LockdownControllerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func oneButton(sender: UIButton) {
+        
+    }
 
+    @IBAction func twoButton(sender: UIButton) {
+    }
+   
+    @IBAction func threeButton(sender: UIButton) {
+    }
+    
+    @IBAction func fourButton(sender: UIButton) {
+    }
+    
+    @IBAction func fiveButton(sender: UIButton) {
+    }
+    
+    @IBAction func sixButton(sender: UIButton) {
+    }
+    
+    @IBAction func sevenButton(sender: UIButton) {
+    }
+    
+    @IBAction func eightButton(sender: UIButton) {
+    }
+    
+    @IBAction func nineButton(sender: UIButton) {
+    }
+    
+    @IBAction func zeroButton(sender: UIButton) {
+    }
+    
+    @IBAction func clearButton(sender: UIButton) {
+    }
+    
+    @IBAction func backButton(sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
